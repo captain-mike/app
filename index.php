e6cca19b29f8ed50494b3c6702820ff44e023b57
captain-mike <info@grafikweb.it>
2022-12-22 03:31:51 +0100

    lang attr

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ $lang = in_array($lang, $acceptLang) ? $lang : 'en';
 require_once "language/{$lang}.php"; 
 ?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="<?=$lang?>">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
