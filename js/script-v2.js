eeb779e2d6b3e229c055aa925bbe71d91cdf9c19
captain-mike <info@grafikweb.it>
2022-12-24 01:12:04 +0100

    compatibility

--- js/script-v2.js
+++ js/script-v2.js
@@ -26,13 +26,13 @@
       let supportwebm = supports_media('video/webm; codecs="vp8, vorbis"');
 
       if(supportmp4)
-      alert(supportmp4);
+      alert('mp4 ' + '+supportmp4);
 
       if(supportogg)
-      alert(supportogg)
+      alert('ogg ' + supportogg)
 
       if(supportwebm)
-      alert(supportwebm)
+      alert('webm ' + supportwebm)
 
     async function load() {
         
